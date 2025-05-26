be3eb2f918d64a8e2b534ff41085b84505921f87
jeryliu <jeryliu@tencent.com>
2025-05-26 10:54:12 +0800

    feat: 新增lint校验

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,14 @@
 import "amfe-flexible";
-import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import { MorningQuestion } from "./component/MorningQuestion";
 import "./styles/global.css";
 
 export const App = () => {
-    return <MorningQuestion />;
+  return <MorningQuestion />;
 };
 
 const container = document.getElementById("root");
 if (container) {
-    const root = ReactDOM.createRoot(container);
-    root.render(<App />);
+  const root = ReactDOM.createRoot(container);
+  root.render(<App />);
 }
